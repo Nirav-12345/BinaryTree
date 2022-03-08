f542e2e774cb4cecab4f5a70a73e087879b1b617
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-08 10:49:09 -0800

    adding all binary files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        binaryTree b = new binaryTree();
+
+        b.Add(56);
+        b.Add(30);
+        b.Add(70);
+       
+        b.TraversePreorder(b.Root);
+    }
+}
