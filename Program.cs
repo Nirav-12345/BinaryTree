26f89123ed0e4e9c635015ebc65cd9cb29586f1d
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-09 10:09:35 -0800

    adding all files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ConsoleApp1;
+namespace BinTree;
 
 class Program
 {
@@ -10,8 +10,18 @@ class Program
 
         b.Add(56);
         b.Add(30);
+        b.Add(40);
+        b.Add(20);
+        b.Add(11);
+        b.Add(3);
+        b.Add(16);
+        b.Add(95);
         b.Add(70);
-       
+        b.Add(65);
+        b.Add(63);
+        b.Add(67);
+        b.Add(70);
+
         b.TraversePreorder(b.Root);
     }
 }
